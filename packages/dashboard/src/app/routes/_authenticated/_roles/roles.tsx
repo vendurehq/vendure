e034ddfaca60dbb7967540e44eb09992f7a868a6
Bibiana Sebestianova <b.sebestianova@gmail.com>
2026-08-24 15:33:51 +0200

    feat(dashboard): Remove role-channel model from dashboard

--- packages/dashboard/src/app/routes/_authenticated/_roles/roles.tsx
+++ packages/dashboard/src/app/routes/_authenticated/_roles/roles.tsx
@@ -1,14 +1,12 @@
-import { ChannelCodeLabel } from '@/vdb/components/shared/channel-code-label.js';
 import { DetailPageButton } from '@/vdb/components/shared/detail-page-button.js';
 import { RoleCodeLabel } from '@/vdb/components/shared/role-code-label.js';
-import { Badge } from '@/vdb/components/ui/badge.js';
 import { Button } from '@/vdb/components/ui/button.js';
 import { CUSTOMER_ROLE_CODE, SUPER_ADMIN_ROLE_CODE } from '@/vdb/constants.js';
 import { ActionBarItem } from '@/vdb/framework/layout-engine/action-bar-item-wrapper.js';
 import { ListPage } from '@/vdb/framework/page/list-page.js';
 import { Trans } from '@lingui/react/macro';
 import { createFileRoute, Link } from '@tanstack/react-router';
-import { LayersIcon, PlusIcon } from 'lucide-react';
+import { PlusIcon } from 'lucide-react';
 import { ExpandablePermissions } from './components/expandable-permissions.js';
 import { DeleteRolesBulkAction } from './components/role-bulk-actions.js';
 import { roleListQuery } from './roles.graphql.js';
@@ -30,7 +28,6 @@ function RoleListPage() {
             defaultVisibility={{
                 description: true,
                 code: true,
-                channels: true,
                 permissions: true,
             }}
             customizeColumns={{
@@ -56,23 +53,6 @@ function RoleListPage() {
                         }
 
                         return <ExpandablePermissions role={row.original} />;
-                    },
-                },
-                channels: {
-                    cell: ({ row }) => {
-                        if (SYSTEM_ROLES.includes(row.original.code)) {
-                            return null;
-                        }
-
-                        return (
-                            <div className="flex flex-wrap gap-2">
-                                {row.original.channels.map(channel => (
-                                    <Badge variant="secondary" key={channel.code}>
-                                        <LayersIcon /> <ChannelCodeLabel code={channel.code} />
-                                    </Badge>
-                                ))}
-                            </div>
-                        );
                     },
                 },
             }}
